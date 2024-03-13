bce6564f46f8c4c3e306a9cbb5c3a10fcbcf6ad4
saleh <animatorsaleh@gmail.com>
2024-03-14 02:26:03 +0330

    Add main page

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      height: ['responsive'], // Include responsive variants for height utilities
+    },
+  },
+  plugins: [],
+};
+export default config;
