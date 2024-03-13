fc76fa0d1b73e887466db0b17ec282c2fd9f0f69
saleh <animatorsaleh@gmail.com>
2024-03-14 03:17:06 +0330

    Remove strick-mode in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-};
-
-export default nextConfig;
-
+    output:'export',
+  };
+  /* module.exports = nextConfig*/
+  export default nextConfig;
