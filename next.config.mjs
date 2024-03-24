697b90e53e9d6e52ea55ce7dca59962ca33370b9
saleh <animatorsaleh@gmail.com>
2024-03-24 19:11:28 +0330

    Use env to determine assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+let assetPrefix = process.env.PortfolioEnv === "production" ? "https://sirsaleh.github.io/Portfolio/" : "";
+
 const nextConfig = {
     output:'export',
+    assetPrefix: assetPrefix,
   };
   /* module.exports = nextConfig*/
   export default nextConfig;
