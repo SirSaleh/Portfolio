751a05f4ad63343b2ca61aa6c832913691b20e2a
saleh <animatorsaleh@gmail.com>
2024-03-28 12:30:10 +0330

    Remove reduntant use effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-"use client";
 
 import Image from "next/image";
 import FirstPage from "./special components/first_page";
 import { useEffect } from "react";
 
 export default function Home() {
-
-  useEffect(function(){
-    console.log("hi");
-  });
-
   return (
     <main>
       <FirstPage></FirstPage>
