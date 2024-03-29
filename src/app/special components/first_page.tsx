7f0566f8b493108e5088ee4a1ce19d65b45d73bc
saleh <animatorsaleh@gmail.com>
2024-03-29 03:54:25 +0330

    Add main skills to first-page

--- src/app/special components/first_page.tsx
+++ src/app/special components/first_page.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
 import PortfolioSinglePager from "../common_components/portfolio_single_pager"
+import SkillItem from "../common_components/SkillItem";
+import { ReactNode } from 'react';
 
 interface PortfolioFirstPageProps {
+    children: ReactNode;
 }
 
 export default function FirstPage(props: PortfolioFirstPageProps) {
@@ -14,6 +16,19 @@ export default function FirstPage(props: PortfolioFirstPageProps) {
             </div>
             <div className="text-white text-2xl align-middle text-center">
               Full-stack developer
+            </div>
+            <div className="skillCloudBox w-1/2 md:w-1/4  flex flex-wrap justify-center content-center mt-3 text-white text-1xl align-middle text-center w-1/2 mx-auto">
+                <SkillItem classNames="!text-white !bg-green-700">Django</SkillItem>
+                <SkillItem classNames="!bg-gray-500">Flask</SkillItem>
+                <SkillItem classNames="!bg-yellow-200 text-black">Javascript pure</SkillItem>
+                <SkillItem classNames="!bg-white text-sky-600">R</SkillItem>
+                <SkillItem classNames="!bg-white text-yellow-600">Linux</SkillItem>
+                <SkillItem classNames="!bg-yellow-500 text-black" title="EC2, S3, and lambda">AWS</SkillItem>
+                <SkillItem classNames="!bg-green-600 text-white">NginX</SkillItem>
+                <SkillItem classNames="!bg-white text-black">NextJS</SkillItem>
+                <SkillItem classNames="!bg-white text-black">Jquery</SkillItem>
+                <SkillItem classNames="!bg-sky-600 text-white">Tailwind CSS</SkillItem>
+                <SkillItem classNames="!bg-violet-600 text-white">Bootstrap CSS</SkillItem>
             </div>
             <div className="absolute bottom-0 left-1/2 md:left-auto md:right-0 md:top-1/2 translate-y-1/2 md:-translate-y-1/2 -translate-x-1/2 md:translate-x-1/2">
               <img src="1.jpg" className="rounded-full"></img>
