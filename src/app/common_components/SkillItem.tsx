b14154937dde6dfb40f35122a8979a46e21ac82f
saleh <animatorsaleh@gmail.com>
2024-04-04 20:33:49 +0330

    Fix sizing bu in skillItem

--- src/app/common_components/SkillItem.tsx
+++ src/app/common_components/SkillItem.tsx
@@ -3,13 +3,13 @@ import { ReactNode } from 'react';
 
 interface SkillItemProps {
     children: ReactNode;
-    classNames: string;
+    classNames?: string | "";
     title?: string | null;
 }
 
 export default function SkillItem(props: SkillItemProps) {
 
-    const ClassNames = "skill-box bg-white text-black m-1 " + props.classNames;
+    const ClassNames = "skill-box bg-white text-black m-1 w-fit h-fit " + props.classNames;
     const titleVal = props.title ? props.title : "";
     return (
     <span className={ClassNames} title={titleVal}>
