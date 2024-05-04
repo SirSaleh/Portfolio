a42ffdd4ef4215bf9ee87b6db947827efdd264cc
saleh <animatorsaleh@gmail.com>
2024-05-04 21:05:11 +0330

    Reduce skill show lag in first page

--- src/app/special components/first_page.tsx
+++ src/app/special components/first_page.tsx
@@ -13,7 +13,7 @@ export default function FirstPage(props: PortfolioFirstPageProps) {
     useEffect(() => {
       const wrapperElement = document.getElementById('skill_holder');
       if (wrapperElement) {
-        animateBrick(wrapperElement, 500, 0);
+        animateBrick(wrapperElement, 200, 0);
       }
     }, []);
 
